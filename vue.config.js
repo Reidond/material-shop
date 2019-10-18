b985dcfd3b14cd37cea9981d4378f80b1d2d6188
Reidond <reidond@gmail.com>
2019-10-18 13:55:05 +0300

    Setup vue

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    devtool: "source-map"
+  },
+  chainWebpack: config => {
+    config.module.rules.delete("scss");
+
+    let scssRule = config.module.rule("scss").test(/\.scss$/);
+
+    [
+      { name: "vue-style-loader" },
+      { name: "css-loader" },
+      { name: "postcss-loader" },
+      { name: "fast-sass-loader" }
+    ].forEach(load => {
+      scssRule
+        .use(load.name)
+        .loader(load.loader || load.name)
+        .options(load.options || {});
+    });
+  }
+};
