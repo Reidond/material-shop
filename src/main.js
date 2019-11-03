e4302ea3d2e50b1b57572cdd409a13becb179986
Reidond <reidond@gmail.com>
2019-11-03 11:14:15 +0200

    Add router

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import { router } from "./routes";
 import { store, modules } from "./store";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
+import VueRouter from "vue-router";
 import VuexCompositionApi from "vuex-composition-api";
 
+Vue.use(VueRouter);
 Vue.use(VuexCompositionApi);
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  router,
   store,
   modules,
   render: h => h(App)
